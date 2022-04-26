83e1b1707f74cf2840b0159262cbd387ce84853e
Paulo Rodrigoe <paulo.martins@uds.com.br>
2022-04-26 16:26:57 -0300

    initial commit

---
+++ js/etapas.js
@@ -0,0 +1,51 @@
+const etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: "Vereador 1",
+        partido: "ABC",
+        fotos: [{ url: "boy.jpg", legenda: "Vereador", isPequeno: false }],
+      },
+
+      {
+        numero: 77225,
+        nome: "Vereador 2",
+        partido: "DEF",
+        fotos: [
+          { url: "girl.png", legenda: "Vereador", isPequeno: false },
+        ],
+      },
+    ],
+  },
+
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 14,
+        nome: "Prefeito 1",
+        vice: "Vice-Prefeito 1",
+        partido: "GHI",
+        fotos: [
+          { url: "boy02.jpg", legenda: "Prefeito", isPequeno: false },
+          { url: "boy03.jpg", legenda: "Vice-Prefeito", isPequeno: true },
+        ],
+      },
+
+      {
+        numero: 66,
+        nome: "Prefeito 2",
+        vice: "Vice Prefeito 2",
+        partido: "JKL",
+        fotos: [
+            { url: "girl02.jpg", legenda: "Prefeito", isPequeno: false },
+            { url: "girl03.png", legenda: "Vice-Prefeito", isPequeno: true },
+        ],
+      },
+    ],
+  },
+];
